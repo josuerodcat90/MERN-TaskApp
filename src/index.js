ceaa947ce80bf4435df36f9298b47c9f7a5063b4
55036662 <zhippuden@gmail.com>
2020-03-24 23:02:56 +0100

    database conection ready.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
+
 const app = require('./app');
-require('dotenv/config');
+require('./utils/database');
 
 ///declare the port
 const PORT = process.env.PORT || 5000;
@@ -7,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 const main = async () => {
 	///launch the server
 	await app.listen(PORT);
-	console.log(`Server running on port >>>${PORT}<<<`);
+	console.log(`>>>Server running on port ${PORT}<<<`);
 };
 
 ///execute main function
