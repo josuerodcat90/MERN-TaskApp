d03d1e572f05a1284cb2fe1b6935dab4e0d1a1bd
55036662 <zhippuden@gmail.com>
2020-03-25 13:00:32 +0100

    App settings setted up.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 const express = require('express');
-
-///declare the app constant
+const cors = require('cors');
 const app = express();
+
+///settings
+const PORT = process.env.PORT || 5000;
+app.set('port', PORT);
+
+///middlewares
+app.use(cors());
+app.use(express.json());
+
+///routes
+app.get('/api/users', (req, res) => {
+	res.send('Users Routes!');
+});
+app.get('/api/notes', (req, res) => {
+	res.send('Notes Routes!');
+});
 
 ///export app
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ require('dotenv').config();
 const app = require('./app');
 require('./utils/database');
 
-///declare the port
-const PORT = process.env.PORT || 5000;
-
+///set main function to launch server
 const main = async () => {
-	///launch the server
-	await app.listen(PORT);
-	console.log(`>>>Server running on port ${PORT}<<<`);
+	await app.listen(app.get('port'));
+	console.log(`>>>Server running on port ${app.get('port')}<<<`);
 };
 
 ///execute main function
